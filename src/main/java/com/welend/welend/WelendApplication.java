90a195e054858a3cbe45c475a60500c0642f51e5
Daniel Joseph <daniel.joseph@cashcall.com.eg>
2023-12-25 11:20:39 +0200

    adding project files

---
+++ src/main/java/com/welend/welend/WelendApplication.java
@@ -0,0 +1,13 @@
+package com.welend.welend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welend/welend/WelendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welend.welend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
