f1745d31d87411b52802b38210d86487af2d224d
memad <mina.emad@cashcall.com.eg>
2023-12-25 16:59:40 +0200

    adding user service

--- src/main/java/com/welend/welend/WelendApplication.java
+++ src/main/java/com/welend/welend/WelendApplication.java
@@ -1,9 +1,20 @@
 package com.welend.welend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class WelendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/welend/welend/controller/PortalController.java
+++ src/main/java/com/welend/welend/controller/PortalController.java
@@ -1,24 +1,33 @@
 package com.welend.welend.controller;
 
+import com.welend.welend.DAO.UserDAO;
 import com.welend.welend.DTO.BaseResponse;
 import com.welend.welend.DTO.UserLogin;
 import com.welend.welend.model.User;
+import com.welend.welend.service.UserService;
 import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigInteger;
+
 @RestController
 @CommonsLog
 @RequestMapping("/api")
+@EnableJpaRepositories
 public class PortalController {
+
+    @Autowired
+    private UserService userService;
     @PostMapping(value = "/login")
     public BaseResponse login(@RequestBody UserLogin request)  {
         try {
             log.info("Enter login function...");
-            User user = new User();
-
+            userService.login();
             return new BaseResponse("success", "login success", request);
         } catch (Exception exception) {
             log.error(exception.getMessage());
