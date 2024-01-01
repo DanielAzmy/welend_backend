3e73ca2578d8e14160a2c6226a5610b742427334
memad <mina.emad@cashcall.com.eg>
2024-01-01 13:37:07 +0200

    complete login

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,25 +1,19 @@
 package DAO;
 
 import Model.User;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import servlets.DbConnection;
 import RequestModel.UserLoginModel;
 import ResponseModel.BaseResponse;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import Enum.ResponseStatus;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-
-<<<<<<< HEAD
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
-import static Utilities.Utility.gson;
-import static Utilities.Utility.jdbcTemplate;
+import servlets.DbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import static Utilities.Utility.gson;
 import static Utilities.Utility.jdbcTemplate;
 
@@ -38,32 +32,46 @@ public class UserDAO<T> {
             ps.setString(4, user.getNidBackSide());
             ps.setString(5, user.getSelfie());
             ps.executeUpdate();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLException("something went wrong");
-        }finally {
+        } finally {
             con.close();
         }
         return 0;
     }
 
+    public static User getUserByEmail(UserLoginModel userLoginModel) {
+        String sql = "select * from public.get_user_by_email(:email);";
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(userLoginModel);
+        String userString = jdbcTemplate.queryForObject(sql, params, String.class);
+        BaseResponse data = gson.fromJson(userString, BaseResponse.class);
+        return mapToUser(data);
+    }
 
-        public static User getUserByEmail (UserLoginModel userLoginModel){
-            String sql = "select * from public.get_user_by_email(:email);";
-            BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(userLoginModel);
-            String userString = jdbcTemplate.queryForObject(sql, params, String.class);
-            Object data = gson.fromJson(userString, Object.class);
-            return mapToUser(data);
+    public static User mapToUser(BaseResponse userRow) {
+        if (!userRow.getStatus().equals("Error")) {
+            User user = new User();
+            JsonArray userData = (JsonArray) userRow.getData();
+            JsonObject jsonObject = userData.get(0).getAsJsonObject();
+
+            user.setId(jsonObject.getAsJsonPrimitive("id").getAsLong());
+            user.setEmail(jsonObject.getAsJsonPrimitive("email").getAsString());
+            user.setPassword(jsonObject.getAsJsonPrimitive("password").getAsString());
+            return user;
+        } else {
+            return null;
         }
+    }
 
-    public User getUser(String columnName , T columnValue) throws SQLException{
-        try{
+    public User getUser(String columnName, T columnValue) throws SQLException {
+        try {
             con = DbConnection.getNewConnection();
-            String query = "select * from project.users where " + columnName + " = '" + columnValue+"'";
+            String query = "select * from project.users where " + columnName + " = '" + columnValue + "'";
             PreparedStatement ps = con.prepareStatement(query);
             ResultSet rs = ps.executeQuery();
             User user = new User();
             boolean check = false;
-            while (rs.next()){
+            while (rs.next()) {
                 check = true;
                 user.setId(rs.getLong("id"));
                 user.setEmail(rs.getString("email"));
@@ -73,54 +81,10 @@ public class UserDAO<T> {
                 user.setSelfie(rs.getString("selfie"));
             }
             return check ? user : null;
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLException("something went wrong");
-        }finally {
+        } finally {
             con.close();
-=======
-
-
-public class UserDAO {
-    public static User getUserByEmail(UserLoginModel userLoginModel) {
-        String sql = "select * from public.get_user_by_email(:email);";
-        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(userLoginModel);
-        String userString = jdbcTemplate.queryForObject(sql, params, String.class);
-        BaseResponse response = gson.fromJson(userString, BaseResponse.class);
-        return mapToUser(response.getData());
-    }
-
-    public static User mapToUser(Object userRow) {
-        if (userRow != null) {
-            User user = new User();
-            JsonArray userData = (JsonArray) userRow;
-            JsonObject jsonObject = userData.get(0).getAsJsonObject();
-
-            user.setId(jsonObject.getAsJsonPrimitive("id").getAsLong());
-            user.setEmail(jsonObject.getAsJsonPrimitive("email").getAsString());
-            user.setPassword(jsonObject.getAsJsonPrimitive("password").getAsString());
-            return user;
-        } else {
-            return null;
->>>>>>> 6c3c7ebd6ba6cce038cc47cb9065fe6702f1b1bc
         }
     }
-
-        public static User mapToUser (Object userRow){
-            User user = new User();
-            LinkedTreeMap userData = (LinkedTreeMap) ((ArrayList<?>) userRow).get(0);
-            long id = Math.round((Double) userData.get("id"));
-            user.setId(id);
-            user.setEmail((String) userData.get("email"));
-            user.setPassword((String) userData.get("password"));
-            user.setArea((String) userData.get("area"));
-            user.setBirthDate((String) userData.get("birth_date"));
-            user.setFullName((String) userData.get("full_name"));
-            user.setFirstName((String) userData.get("first_name"));
-            user.setGovernorate((String) userData.get("governorate"));
-            user.setNidSerialNumber((String) userData.get("nid_serial_number"));
-            user.setGender((String) userData.get("gender"));
-            return user;
-        }
-
 }
-
--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,76 +1,59 @@
 package Service;
 
+import DAO.SessionDAO;
 import DAO.UserDAO;
 import DTO.UserRegisterInputDTO;
+import Enum.ResponseStatus;
+import Model.Session;
 import Model.User;
 import RequestModel.UserLoginModel;
-<<<<<<< HEAD
 import ResponseModel.BaseResponse;
-import com.sun.mail.iap.Response;
-import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import DAO.SessionDAO;
-import Model.Session;
-import java.sql.SQLException;
-import static Utilities.Utility.gson;
-import static Utilities.Utility.jdbcTemplate;
-import  Enum.ResponseStatus;
-
-    public class UserService {
-    static UserDAO userDAO  = new UserDAO();
-        public static String login(UserLoginModel userLoginModel) throws SQLException {
-            User user = UserDAO.getUserByEmail(userLoginModel);
-
-            Session session = JWTService.generateToken(user.getId());
-            if (session == null) {
-                throw new SQLException("Error in session creation.");
-            }
-            SessionDAO.saveSession(session);
-            return session.toString();
-        }
-
-        public static String userRegister(UserRegisterInputDTO userRegisterInputDTO) throws SQLException {
-            try {
-                userRegisterInputDTO.setPassword(BCrypt.hashpw(userRegisterInputDTO.getPassword(), BCrypt.gensalt()));
-
-                User user = new User(userRegisterInputDTO.getEmail(),
-                        userRegisterInputDTO.getPassword(),
-                        userRegisterInputDTO.getNidFrontSide(),
-                        userRegisterInputDTO.getnidBackSide(),
-                        userRegisterInputDTO.getSelfie()
-                );
-                if (userDAO.register(user) == 1)
-                {
-                    throw new SQLException("something went wrong");
-                }else
-                        return ResponseStatus.Success.toString();
-
-            } catch (SQLException e) {
-                throw new SQLException("this email or username is already taken");
-            }
-=======
-import Enum.ResponseStatus;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.SQLException;
-import java.util.logging.Level;
 
-import static Utilities.Utility.*;
+import static Utilities.Utility.log;
 
 public class UserService {
-    public static String login(UserLoginModel userLoginModel) throws SQLException {
+    private static final UserDAO userDAO = new UserDAO();
+
+    public static String login(UserLoginModel userLoginModel) throws Exception {
         User user = UserDAO.getUserByEmail(userLoginModel);
-        if(user == null || !BCrypt.checkpw(userLoginModel.getPassword(), user.getPassword())){
-            throw new SQLException("wrong credentials");
-        }
->>>>>>> 6c3c7ebd6ba6cce038cc47cb9065fe6702f1b1bc
 
+        if (user == null) {
+            throw new Exception("Invalid Email or Password.");
         }
-<<<<<<< HEAD
-=======
 
+        if (!BCrypt.checkpw(userLoginModel.getPassword(), user.getPassword())) {
+            throw new Exception("Invalid Email or Password.");
+        }
+
+        Session session = JWTService.generateToken(user.getId());
+        if (session == null) {
+            throw new SQLException("Error in session creation.");
+        }
         SessionDAO.saveSession(session);
         return session.getToken();
->>>>>>> 6c3c7ebd6ba6cce038cc47cb9065fe6702f1b1bc
     }
 
+    public static String userRegister(UserRegisterInputDTO userRegisterInputDTO) throws SQLException {
+        try {
+            log.info("Enter userRegister function...");
+            userRegisterInputDTO.setPassword(BCrypt.hashpw(userRegisterInputDTO.getPassword(), BCrypt.gensalt()));
+
+            User user = new User(userRegisterInputDTO.getEmail(),
+                    userRegisterInputDTO.getPassword(),
+                    userRegisterInputDTO.getNidFrontSide(),
+                    userRegisterInputDTO.getnidBackSide(),
+                    userRegisterInputDTO.getSelfie()
+            );
+            if (userDAO.register(user) == 1) {
+                throw new SQLException("Something went wrong");
+            } else
+                return ResponseStatus.Success.toString();
+
+        } catch (SQLException e) {
+            throw new SQLException("This Email is already Exist.");
+        }
+    }
+}
